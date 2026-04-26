df6f5105a5db2b63f055f110fc72537971596a6d
khemiriahmed <khemiriahmed6@gmail.com>
2026-04-26 12:18:48 +0100

    Api update password

--- frontend/src/services/api/auth.js
+++ frontend/src/services/api/auth.js
@@ -4,3 +4,4 @@ export const register = (data) => api.post("/register", data);
 export const login = (data) => api.post("/login", data);
 export const getUser = () => api.get("/user");
 export const logout = () => api.post("/logout");
+export const updatePassword = (data) => api.post("/profile/password", data);
