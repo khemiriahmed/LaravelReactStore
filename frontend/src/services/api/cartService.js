951904595acde8e45ec7bdabd404697b34ede326
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-07 18:49:22 +0100

    add file cartService

---
+++ frontend/src/services/api/cartService.js
@@ -0,0 +1,34 @@
+import api from "../api/axios";
+
+// GET CART
+export const getCart = async () => {
+  const res = await api.get("/cart");
+  return res.data.data;
+};
+
+// ADD ITEM
+export const addToCart = async (data) => {
+  const res = await api.post("/cart/items", data);
+  return res.data.data;
+};
+
+// UPDATE ITEM
+export const updateCartItem = async (id, quantity) => {
+  const res = await api.put(`/cart/items/${id}`, {
+    quantity,
+  });
+
+  return res.data.data;
+};
+
+// REMOVE ITEM
+export const removeCartItem = async (id) => {
+  const res = await api.delete(`/cart/items/${id}`);
+  return res.data.data;
+};
+
+// CLEAR CART
+export const clearCart = async () => {
+  const res = await api.delete("/cart/clear");
+  return res.data.data;
+};
