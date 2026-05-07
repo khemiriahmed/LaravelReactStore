039581e693632ded329d31a55d37747f50cc419f
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-07 18:52:54 +0100

    Config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ProductProvider } from "./context/ProductContext";
 import { CategoryProvider } from "./context/CategoryContext";
+import { CartProvider } from "./context/CartContext";
 import "./App.css";
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
       <AuthProvider>
         <ProductProvider>
           <CategoryProvider>
+            <CartProvider>
             <RouterProvider router={router} />
+            </CartProvider>
           </CategoryProvider>
         </ProductProvider>
       </AuthProvider>
